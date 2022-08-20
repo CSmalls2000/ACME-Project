44c80125dfc7fd48b789f1bd12bab03494de3d1a
CSmalls2000 <70129042+CSmalls2000@users.noreply.github.com>
2022-08-20 17:56:11 -0400

    Made a mistake and added to wrong branch

--- src/utils/Foo.java
+++ src/utils/Foo.java
@@ -5,8 +5,6 @@ public class Foo {
 	public static void main(String[] args) {
 		System.out.println("howdy");
 		System.out.println("Hi from GitHub");
-		System.out.println("hi 'cynt' branch, from Eclipse");
-		System.out.println("hello worlds");
 	}
 
 }
