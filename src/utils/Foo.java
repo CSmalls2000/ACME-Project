ec6f0e651e7baebf0b4ce11de93926e8b441b41e
Cynthia Smalls <cynthia.smalls14@gmail.com>
2022-08-21 15:13:13 -0400

    A commit for tutorial screenshot

--- src/utils/Foo.java
+++ src/utils/Foo.java
@@ -7,7 +7,7 @@ public class Foo {
 		System.out.println("Hi from GitHub");
 		System.out.println("hi 'cynt' branch, from Eclipse");
 		System.out.println("hello worlds"); 
-		System.out.println("hello 'person2', From Eclipse");
+		System.out.println("hello 'person2', From Eclipse"); 
 	}
 
 }
